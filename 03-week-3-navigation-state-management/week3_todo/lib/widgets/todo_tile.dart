06c5261747a3d0ce991eeac430e5285bb6c29280
Aisya Aswy Nur Aidha <aisyaaidha@gmail.com>
2026-09-12 16:42:57 +0700

    refactoring TodoTile

---
+++ 03-week-3-navigation-state-management/week3_todo/lib/widgets/todo_tile.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../providers/todo_provider.dart';
+
+/// Widget terpisah untuk menampilkan satu item tugas dalam daftar ToDo.
+class TodoTile extends ConsumerWidget {
+  final Todo todo;
+  final int index;
+
+  const TodoTile({
+    super.key,
+    required this.todo,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ListTile(
+      leading: Checkbox(
+        value: todo.done,
+        onChanged: (_) =>
+            ref.read(todoListProvider.notifier).toggle(index),
+      ),
+      title: Text(
+        todo.title,
+        style: TextStyle(
+          decoration: todo.done ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: IconButton(
+        icon: const Icon(Icons.delete),
+        onPressed: () =>
+            ref.read(todoListProvider.notifier).remove(index),
+      ),
+    );
+  }
+}
